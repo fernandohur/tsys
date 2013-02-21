268360f9d5316ee4d3d9e16f8f46258fd709053e
fernandohur <fernandohur@gmail..com>
2013-02-21 15:47:21 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tsys::Application.config.secret_token = 'e1f290e91f4b38b96a75799d343eb24b354bcbc9111964a8fbc959fbb3b50368f71e3c6e437b3503d890a820cebb9243c2d2698b4dd1c9c876e885729094acbe'
