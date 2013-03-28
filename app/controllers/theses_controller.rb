a651fbb6d0f9010842eda128a35e05fd3384f0d1
Adriana Pineda <am.pineda206@uniandes.edu.co>
2013-03-28 11:14:28 -0500

    ldap

--- app/controllers/theses_controller.rb
+++ app/controllers/theses_controller.rb
@@ -119,19 +119,10 @@ class ThesesController < ApplicationController
     require 'rubygems'
     require 'net/ldap'
     ldap = Net::LDAP.new
-    ldap.host = "x500.bund.de"
-    ldap.port = "389"
+    ldap.host = "localhost"
+    ldap.port = "10389"
     ldap.auth username, password
     is_authorized = ldap.bind
-
-    puts "---------------------------------"
-    puts is_authorized
-    puts "---------------------------------"
-
-    attrs = []
-    ldap.search( :base => "l=init,ou=Service,o=Bund,c=DE", :attributes => attrs, :return_result => true ) do |entry|
-    # entry is records returned after search
-    end
 
     is_authorized = true
     if is_authorized==true
