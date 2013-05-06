cf928a46b6366aa43d2c3b77d28fa1566275a3d7
Adriana Pineda <am.pineda206@uniandes.edu.co>
2013-05-05 23:14:13 -0500

    pruebas

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,11 +3,10 @@ class Product < ActiveRecord::Base
   validates_uniqueness_of :name, :message => "Sorry, you can't have two or more products with the same name."
   belongs_to :Student
   has_one :activity
-  default_scope order('created_at DESC')
 
   def self.search(search)
     if search
-      find(:all,:conditions => ['name LIKE ?',"%#{search}%"])
+      find(:all,:conditions => ['name LIKE ?',"%#{search}%"], :order => "name")
     else
       find(:all)
     end
