54e7f7ef72b43e8b7c6ff173831292e5372cd407
fernandohur <fernandohur@gmail.com>
2013-05-07 14:50:10 -0500

    added helper methods for session handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  def get_user_id
+    session[:user_id]
+  end
+
+  def is_prof
+    session[:user_type]==:professor
+  end
+
+  def is_student
+    session[:user_type]==:student
+  end
+
+  def send_home_with_flash(flash_msg)
+    flash[:notice]=flash_msg
+    redirect_to '/'
+  end
+
 end
