9b1796432254be9a34eb67687135ac1bbca12da0
fernandohur <fernandohur@gmail.com>
2013-05-07 18:05:55 -0500

    added test get_students

--- test/unit/professor_unit_test.rb
+++ test/unit/professor_unit_test.rb
@@ -35,4 +35,31 @@ class ProfessorTest < ActiveSupport::TestCase
 
   end
 
+  test ' testing get_students ' do
+    Professor.delete_all
+    Student.delete_all
+    Thesis.delete_all
+
+    p = Professor.new(:nombre=>'name',:password=>'password',:username=>'username')
+    p.save!
+
+    s1 = Student.new(:name=>'name',:password=>'password',:username=>'username')
+    s1.save!
+
+    t1 = Thesis.new(:name=>'mondaquera',:student_id=>s.id)
+    t1.save!
+
+    s2 = Student.new(:name=>'name',:password=>'password',:username=>'username')
+    s2.save!
+
+    t2 = Thesis.new(:name=>'mondaquera',:student_id=>s.id)
+    t2.save!
+
+    students = p.get_students
+    assert students.size==2
+
+  end
+
+
+
 end
